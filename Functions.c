9d0fe5967617f280883000742b06197a41fd0224
Saurabh Jadhav <108616952+Jadhavsau777@users.noreply.github.com>
2024-04-14 18:58:39 +0530

    Update Functions.c

--- Functions.c
+++ Functions.c
@@ -1,53 +1,46 @@
 #include <stdio.h>
 
-int addition(int x, int y)
-{
+int addition(int x, int y) {
     int sum;
     sum = x + y;
     return sum;
 }
 
-int multi(int x, int y)
-{
+int multi(int x, int y) {
     int sum;
     sum = x * y;
     return sum;
 }
 
-float division(int x, int y)
-{
+float division(int x, int y) {
     float sum;
     sum = (float)x / y;
     return sum;
 }
 
-int main()
-{
+int main() {
     char ch;
-    do
-    {
-        int a, b,c,d,e;
-        printf("Enter Any two Numbers\n");
+    do {
+        int a, b, c, d;
+        float e;
+        printf("Enter any two numbers:\n");
         scanf("%d %d", &a, &b);
 
-        c=addition(a, b);
-        d=multi(a, b);
-        e=division(a, b);
+        c = addition(a, b);
+        d = multi(a, b);
+        e = division(a, b);
 
-        printf("The Addition Is:",c);
-        printf("The Multiplication Is:",d)l
-        printf("The Division Is:",e);
+        printf("The Addition Is: %d\n", c);
+        printf("The Multiplication Is: %d\n", d);
+        printf("The Division Is: %.2f\n", e);
 
-        printf("Do You Want to Do Another Number Y/N?\n");
+        printf("Do you want to do another number? (Y/N)\n");
         fflush(stdin);
         scanf(" %c", &ch);
-
     } while (ch == 'Y' || ch == 'y');
-    
-    if(ch=='n' ||ch=='N');
-    {
-    printf("ThankYou!");
-    }
+
+    printf("Thank you!\n");
 
     return 0;
 }
+
